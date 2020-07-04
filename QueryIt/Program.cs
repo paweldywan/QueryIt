6a1bceca019516508a1bc7f87a7f4e9bad849585
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-07-05 00:46:46 +0200

    Development

--- QueryIt/Program.cs
+++ QueryIt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,63 @@ namespace QueryIt
     {
         static void Main(string[] args)
         {
+            Converter<Employee, Employee> converter = ConvertIt;
+
+            var c = converter(new Employee());
+
+            Database.SetInitializer(new DropCreateDatabaseAlways<EmployeeDb>());
+
+            using (IRepository<Employee> employeeRepository = new SqlRepository<Employee>(new EmployeeDb()))
+            {
+                AddEmployees(employeeRepository);
+                AddManagers(employeeRepository);
+                CountEmployees(employeeRepository);
+                QueryEmployees(employeeRepository);
+                DumpPeople(employeeRepository);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static Manager ConvertIt(Person person)
+        {
+            return new Manager();
+        }
+
+        private static void AddManagers(IWriteOnlyRepository<Manager> employeeRepository) //Do mniej szczegółowych typów zostanie dodany bardziej szczegółowy
+        {
+            employeeRepository.Add(new Manager { Name = "Alex" });
+            employeeRepository.Commit();
+        }
+
+        private static void DumpPeople(IReadOnlyRepository<Person> employeeRepository) //Bardziej szczegółowe typy zostaną zwrócone jako mniej szczegółowe
+        {
+            var employees = employeeRepository.FindAll();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }
+
+        private static void QueryEmployees(IRepository<Employee> employeeRepository)
+        {
+            var employee = employeeRepository.FindById(1);
+
+            Console.WriteLine(employee.Name);
+        }
+
+        private static void CountEmployees(IRepository<Employee> employeeRepository)
+        {
+            Console.WriteLine(employeeRepository.FindAll().Count());
+        }
+
+        private static void AddEmployees(IRepository<Employee> employeeRepository)
+        {
+            employeeRepository.Add(new Employee { Name = "Scott" });
+            employeeRepository.Add(new Employee { Name = "Chris" });
+
+            employeeRepository.Commit();
         }
     }
 }
